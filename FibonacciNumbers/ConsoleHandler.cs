e86d8d6306c4dec59a365720db28c2148747e3f5
Aliaksandr Liashevich <aliaksandr.liashevich@gmail.com>
2019-02-04 19:43:24 +0300

    Release first Fibonacci method

---
+++ FibonacciNumbers/ConsoleHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    public class ConsoleHandler
+    {
+        public void CalculateNumber()
+        {
+            int position = GetPosition();
+
+            Console.Write("Answer: ");
+
+            Console.WriteLine(FibonacciNumbers.Fibonacci(position));
+        }
+
+        private int GetPosition()
+        {
+            Console.WriteLine("Please, enter a value");
+
+            int.TryParse(Console.ReadLine(), out var result);
+
+            return result;
+        }
+    }
+}
---
+++ FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    public static class FibonacciNumbers
+    {
+        public static double Fibonacci(int n)
+        {
+            double root = Math.Sqrt(5);
+
+            double fi_plus = (1 + root) / 2;
+
+            double fi_minus = (1 - root) / 2;
+
+            double result = (Math.Pow(fi_plus, n) - Math.Pow(fi_minus, n)) / root;
+
+            return result;
+        }
+    }
+}
